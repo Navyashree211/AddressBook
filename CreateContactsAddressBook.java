30eabceb8820606b4e16dcd7dba39a3e54d1bb25
navyas211@gmail.com <navyas211@gmail.com>
2021-10-03 21:00:49 +0530

    [ADDED]: Create a Contact Details For Address Book

---
+++ CreateContactsAddressBook.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbook;
+
+public class CreateContactsAddressBook {   
+	public static void main(String[] args) {   
+		
+		ContactsDetails obj = new ContactsDetails("Navya" , "Shree", "Ratu Road", "Ranchi", "Jharkhand", "navyas211@gmail.com", 834002, 894921 );
+		
+		System.out.println(obj);
+	}
+
+	
+	   
+
+}
